9741de22f9102271d6f53a015d2001367ee4d717
Kairav <kairavdutta@gmail.com>
2026-04-10 22:55:26 +0530

    Tried adding left and right arrow key (still bugy)

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]) { // TODO: User input of filename as argv.
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
     Buffer buf = Buffer();
 
-    cout << "Press keys: Esc +  ':q + enter' to quit):\n";
+    cout << "Press keys: Esc +  ':q + enter' to quit):";
     // int cursor_row = 0, cursor_col = 0;
     while (true) {
         cout << "\033[H";          // move cursor to top-left
@@ -28,7 +28,7 @@ int main(int argc, char* argv[]) { // TODO: User input of filename as argv.
         char c;
         read(STDIN_FILENO, &c, 1);
 
-        if (c == 27) { // ESC
+        if (c == ESCAPE) { 
             char next;
             read(STDIN_FILENO, &next, 1);
             if (next == ':') {
@@ -42,8 +42,16 @@ int main(int argc, char* argv[]) { // TODO: User input of filename as argv.
                 }
                 if (command == "q") break;
             }
+            else if (next == '[') {
+                char dir;
+                read(STDIN_FILENO, &dir, 1);
+                if (dir == 'C') 
+                    buf.cursorRight();
+                else if (dir == 'D') 
+                    buf.cursorLeft();
+            }
         }
-        else if (c == 127 || c == 8) {
+        else if (c == BACKSPACE || c == 8) {
             buf.backspace();
         }
         else {
